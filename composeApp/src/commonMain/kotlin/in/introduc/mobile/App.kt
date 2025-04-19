f8bd984d6d0c2cf91bf62f32fcfcf6a5b96267c1
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2025-04-20 02:45:31 +0530

    App bar added.

--- composeApp/src/commonMain/kotlin/in/introduc/mobile/App.kt
+++ composeApp/src/commonMain/kotlin/in/introduc/mobile/App.kt
@@ -19,6 +19,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -33,6 +41,21 @@ fun App() {
     var currentRoute by remember { mutableStateOf(Screen.Feed.route) }
 
     Scaffold(
+        modifier = Modifier.padding(top = 24.dp), // Add padding for the status bar
+        topBar = {
+            TopAppBar(
+                title = { Text("App Title") },
+                actions = {
+                    IconButton(onClick = { /* Handle profile icon click */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Person,
+                            contentDescription = "Profile Icon",
+                            tint = Color.White
+                        )
+                    }
+                }
+            )
+        },
         bottomBar = {
             BottomNavBar(
                 currentRoute = currentRoute,
